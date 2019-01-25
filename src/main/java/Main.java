aee52a84938e3552b80d0f339589685f0dc3780a
KostyaHomich <kostaykhomich87@gmail.com>
2019-01-26 00:40:29 +0300

    Delete Main.java

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import com.epam.logisticsbase.controller.LogisticBaseController;
-
-public class Main {
-    public static void main(String[] args) {
-        LogisticBaseController logisticBaseController=LogisticBaseController.getInstance();
-        logisticBaseController.readFileAndStartThreads("src/test/resources/Cars.xml","src/main/resources/Cars.xsd",3);
-    }
-}
